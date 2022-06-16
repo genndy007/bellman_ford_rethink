97a5b514118409d57915bed8f8ab5866a03e9e9e
genndy007 <kochevgenazp@gmail.com>
2022-06-16 21:11:40 +0300

    helloworld

---
+++ src/main/java/com/github/genndy007/bellman_ford/BellmanFordApplication.java
@@ -0,0 +1,7 @@
+package com.github.genndy007.bellman_ford;
+
+public class BellmanFordApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
