bffdcf15671eb019869be9d4ab7a64edb1db6c3d
genndy007 <kochevgenazp@gmail.com>
2022-06-17 00:12:08 +0300

    Create vertex with id and dist

---
+++ src/main/java/com/github/genndy007/bellman_ford/Vertex.java
@@ -0,0 +1,10 @@
+package com.github.genndy007.bellman_ford;
+
+public class Vertex {
+    public final int id;
+    public int distance;
+
+    public Vertex(int id) {
+        this.id = id;
+    }
+}
