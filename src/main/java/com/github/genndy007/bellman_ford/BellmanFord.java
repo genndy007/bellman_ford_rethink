44094f1fc7f1ed25307a3b1cf009d08c34555f1d
genndy007 <kochevgenazp@gmail.com>
2022-06-18 22:05:15 +0300

    Add optional optimizations

--- src/main/java/com/github/genndy007/bellman_ford/BellmanFord.java
+++ src/main/java/com/github/genndy007/bellman_ford/BellmanFord.java
@@ -3,9 +3,10 @@ package com.github.genndy007.bellman_ford;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class BellmanFord {
     public Graph graph;
@@ -32,18 +33,45 @@ public class BellmanFord {
         }
     }
 
-    public void parallelAlgorithm(int numberOfThreads) {
-        MyThreadPool threadPool = new MyThreadPool(numberOfThreads, 1000000);
-
+    public void sequentialAlgorithmWithCheck() {
         for (Vertex v : graph.vertices) {
-            threadPool.execute(() -> {
-                v.distance = Integer.MAX_VALUE - 100;
-            });
+            v.distance = Integer.MAX_VALUE - 100;
         }
-        threadPool.waitUntilAllTasksFinished();
         graph.vertices.get(startId).distance = 0;
 
+        boolean distUpdated = false;
+        for (int i = 1; i < graph.vertices.size(); i++) {
+            distUpdated = false;
+            for (Edge e : graph.edges) {
+                int distBefore = graph.vertices.get(e.endId).distance;
+                graph.vertices.get(e.endId).distance = Math.min(
+                        graph.vertices.get(e.endId).distance,
+                        graph.vertices.get(e.startId).distance + e.weight
+                );
+                if (distBefore != graph.vertices.get(e.endId).distance && !distUpdated) {
+                    distUpdated = true;
+                }
+            }
+            if (!distUpdated) break;
+        }
+    }
 
+    public void parallelAlgorithm(int numberOfThreads) {
+        MyThreadPool threadPool = new MyThreadPool(numberOfThreads, 1000000);
+        long startTimeInf = System.currentTimeMillis();
+        for (Vertex v : graph.vertices) {
+//            threadPool.execute(() -> {
+            v.distance = Integer.MAX_VALUE - 100;
+//            });
+        }
+//        threadPool.waitUntilAllTasksFinished();
+        graph.vertices.get(startId).distance = 0;
+
+        long endTimeInf = System.currentTimeMillis();
+        System.out.println("infset took " + (endTimeInf - startTimeInf) + " milliseconds");
+
+
+        long startTime = System.currentTimeMillis();
         // prepare chunks containers
         List<List<Edge>> adjEdges = new ArrayList<>();
         int chunkSize = graph.vertices.size() / numberOfThreads;
@@ -66,6 +94,8 @@ public class BellmanFord {
             }
         }
 //        System.out.println("Mapping over");
+        long endTime = System.currentTimeMillis();
+        System.out.println("adjseq took " + (endTime - startTime) + " milliseconds");
 
 
 //        System.out.println(adjEdges);
@@ -90,6 +120,79 @@ public class BellmanFord {
             }
         }
         threadPool.stop();
+
+
+    }
+
+
+    public void parallelAlgorithmExecutorService(int numberOfThreads) throws InterruptedException {
+        for (Vertex v : graph.vertices) {
+            v.distance = Integer.MAX_VALUE - 100;
+        }
+        graph.vertices.get(startId).distance = 0;
+
+        // prepare chunks containers
+        List<List<Edge>> adjEdges = new ArrayList<>();
+        int chunkSize = graph.vertices.size() / numberOfThreads;
+        List<IntPair> chunks = new ArrayList<>();
+        for (int i = 0; i * chunkSize < graph.vertices.size(); i++) {
+            chunks.add(new IntPair(i * chunkSize, i * chunkSize + chunkSize));
+            adjEdges.add(new ArrayList<>());
+        }
+
+        // split edges into parallelizable and seq
+        List<Edge> seqEdges = new ArrayList<>(graph.edges);
+        for (Edge e : graph.edges) {
+            for (int i = 0; i < chunks.size(); i++) {
+                IntPair pair = chunks.get(i);
+                if (e.startId >= pair.x && e.startId < pair.y && e.endId >= pair.x && e.endId < pair.y) {
+                    adjEdges.get(i).add(e);
+                    seqEdges.remove(e);
+                    break;
+                }
+            }
+        }
+
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+        List<Callable<String>> tasks = new ArrayList<>();
+        AtomicBoolean distUpdated = new AtomicBoolean(false);
+        for (int i = 1; i < graph.vertices.size(); i++) {
+            distUpdated.set(false);
+            tasks.clear();
+            for (List<Edge> queue : adjEdges) {
+                tasks.add(() -> {
+                    for (Edge e : queue) {
+                        int distBefore = graph.vertices.get(e.endId).distance;
+                        graph.vertices.get(e.endId).distance = Math.min(
+                                graph.vertices.get(e.endId).distance,
+                                graph.vertices.get(e.startId).distance + e.weight
+                        );
+
+                        if (distBefore != graph.vertices.get(e.endId).distance && !distUpdated.get()) {
+                            distUpdated.set(true);
+                        }
+                    }
+                    return "null";
+                });
+            }
+            executorService.invokeAll(tasks);
+
+            for (Edge e : seqEdges) {
+                int distBefore = graph.vertices.get(e.endId).distance;
+                graph.vertices.get(e.endId).distance = Math.min(
+                        graph.vertices.get(e.endId).distance,
+                        graph.vertices.get(e.startId).distance + e.weight
+                );
+                if (distBefore != graph.vertices.get(e.endId).distance && !distUpdated.get()) {
+                    distUpdated.set(true);
+                }
+            }
+
+            if (!distUpdated.get()) {
+                break;
+            }
+        }
+        executorService.shutdown();
 
 
     }
--- src/main/java/com/github/genndy007/bellman_ford/Main.java
+++ src/main/java/com/github/genndy007/bellman_ford/Main.java
@@ -3,7 +3,7 @@ package com.github.genndy007.bellman_ford;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         String graphFilePath = "graph_v1000.txt";
         GraphReader gr = new GraphReader(graphFilePath);
         Graph g = gr.readAsEdgeList();
@@ -11,9 +11,11 @@ public class Main {
         BellmanFord bf = new BellmanFord(g, 0);
 
         long startTime = System.currentTimeMillis();
-        bf.parallelAlgorithm(16);
-//        bf.sequentialAlgorithm();
+//        bf.parallelAlgorithm(16);
+//        bf.sequentialAlgorithmWithCheck();
+        bf.parallelAlgorithmExecutorService(8);
         long endTime = System.currentTimeMillis();
+        g.printDistances();
 
         System.out.println("That took " + (endTime - startTime) + " milliseconds");
 
